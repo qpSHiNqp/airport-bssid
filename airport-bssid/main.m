07ec5cf1b5874794e35105931e51218d42d77e3f
Shintaro Tanaka <shin.bassman@gmail.com>
2013-06-11 21:30:38 +0900

    Initial Commit

---
+++ airport-bssid/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  airport-bssid
+//
+//  Created by Shintaro Tanaka on 6/11/13.
+//  Copyright (c) 2013 Shintaro Tanaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
