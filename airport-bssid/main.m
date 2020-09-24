0e3de464e21a78c2c604cb95ec4b2705c560b097
Shintaro Tanaka <tallow@dirt.ninja>
2020-09-24 12:20:59 +0900

    Call requestAlwaysAuthorization on CLLocationManager instance for bssid scan (macOS 10.15 compatibility)

--- airport-bssid/main.m
+++ airport-bssid/main.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreWLAN/CoreWLAN.h>
+#import <CoreLocation/CoreLocation.h>
 
 NSString *usage_string(NSString *arg0) {
     return [NSString stringWithFormat:@"usage: %@ <ifname> [<bssid>] [<password>]", arg0];
@@ -32,12 +33,74 @@ char const *phyModeName( enum CWPHYMode n )
     }
 }
 
+
+@interface MyScanner : NSObject <CLLocationManagerDelegate>
+
+@property (strong) NSString *bssid;
+@property (strong) CWInterface *interface;
+@property (strong) NSString *password;
+@property (nonatomic) const char *progname;
+
+
+@end
+
+@implementation MyScanner
+
+- (void)locationManager:(CLLocationManager *)locationManager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status == kCLAuthorizationStatusAuthorized) {
+        [self doScan];
+    } else {
+        printf("location authorization did not succeeded\n");
+    }
+}
+
+- (void)doScan {
+    // search for target bssid
+    NSError *error = nil;
+    NSSortDescriptor *nameDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"ssid" ascending:YES];
+    NSArray *scan = [[self.interface scanForNetworksWithSSID:nil error:&error] sortedArrayUsingDescriptors:[NSArray arrayWithObject:nameDescriptor]];
+    if (error)
+        dump_error([NSString stringWithFormat:@"An error has been occurred while scanning networks: %@", error]);
+    CWNetwork *targetNetwork = nil;
+    
+    printf("\x1B[0m***** Scanned networks *****\n");
+    printf("%24s, %17s, %3s, RSSI(dBm)\n", "ESSID", "BSSID", "Ch");
+    for (CWNetwork *network in scan) {
+        if ([network.bssid isEqualToString:self.bssid]) {
+            targetNetwork = network;
+            printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[32m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+        } else {
+            printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[0m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+        }
+    }
+    printf("\x1B[0m****************************\n");
+
+    if (self.bssid == nil) {
+        dump_error([NSString stringWithFormat:@"Network scan completed. If you want to connect to specific BSSID, please enter the command below:\n %@ <ifname> <bssid> [<password>]", [NSString stringWithUTF8String:self.progname]]);
+    }
+
+    if (targetNetwork == nil)
+        dump_error([NSString stringWithFormat:@"The target network \"%@\" could not be found.", self.bssid]);
+    
+    // connection trial
+    BOOL result = [self.interface associateToNetwork:targetNetwork password:self.password error:&error];
+    
+    if (error)
+        dump_error([NSString stringWithFormat:@"Could not connect to the network: %@", error]);
+    
+    if( !result )
+        dump_error(@"Could not connect to the network!");
+    
+    printf("Associated to network \"%s\" (BSSID: %s)\n", [targetNetwork.ssid cStringUsingEncoding:NSUTF8StringEncoding], [self.bssid cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+@end
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         /**
-         * Plan of improvement
+         * Improvement ideas
          *
          * <<argument handling>>
          * -h, --help: show help
@@ -71,43 +134,28 @@ int main(int argc, const char * argv[])
         
         printf("Notice: The interface %s is in %s phyMode.\n", [interfaceName cStringUsingEncoding:NSUTF8StringEncoding], phyModeName(interface.activePHYMode));
         
-        // search for target bssid
-        NSError *error = nil;
-        NSSortDescriptor *nameDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"ssid" ascending:YES];
-        NSArray *scan = [[interface scanForNetworksWithSSID:nil error:&error] sortedArrayUsingDescriptors:[NSArray arrayWithObject:nameDescriptor]];
-        if (error)
-            dump_error([NSString stringWithFormat:@"An error has been occurred while scanning networks: %@", error]);
-        CWNetwork *targetNetwork = nil;
-        
-        printf("\x1B[0m***** Scanned networks *****\n");
-        printf("%24s, %17s, %3s, RSSI(dBm)\n", "ESSID", "BSSID", "Ch");
-        for (CWNetwork *network in scan) {
-            if ([network.bssid isEqualToString:bssid]) {
-                targetNetwork = network;
-                printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[32m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+        MyScanner *scanner = [[MyScanner alloc] init];
+        scanner.bssid = bssid;
+        scanner.password = password;
+        scanner.interface = interface;
+        scanner.progname = argv[0];
+
+        // Location authorization
+        if (@available(macOS 10.15, *)) {
+            CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+            if (status == kCLAuthorizationStatusNotDetermined) {
+                CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+                locationManager.delegate = scanner;
+                [locationManager requestAlwaysAuthorization];
+            } else if (status == kCLAuthorizationStatusAuthorized) {
+                [scanner doScan];
             } else {
-                printf("%s%24s, %17s, %3lu, %3ld\n", "\x1B[0m", [network.ssid cStringUsingEncoding:NSUTF8StringEncoding], [network.bssid cStringUsingEncoding:NSUTF8StringEncoding], (unsigned long)network.wlanChannel.channelNumber, (long)network.rssiValue);
+                printf("Location authorization did not succeeded\n");
             }
+        } else {
+            [scanner doScan];
         }
-        printf("\x1B[0m****************************\n");
-
-        if (bssid == nil) {
-            dump_error([NSString stringWithFormat:@"Network scan completed. If you want to connect to specific BSSID, please enter the command below:\n %@ <ifname> <bssid> [<password>]", [NSString stringWithUTF8String:argv[0]]]);
-        }
-
-        if (targetNetwork == nil)
-            dump_error([NSString stringWithFormat:@"The target network \"%@\" could not be found.", bssid]);
-        
-        // connection trial
-        BOOL result = [interface associateToNetwork:targetNetwork password:password error:&error];
-        
-        if (error)
-            dump_error([NSString stringWithFormat:@"Could not connect to the network: %@", error]);
-        
-        if( !result )
-			dump_error(@"Could not connect to the network!");
-        
-        printf("Associated to network \"%s\" (BSSID: %s)\n", [targetNetwork.ssid cStringUsingEncoding:NSUTF8StringEncoding], [bssid cStringUsingEncoding:NSUTF8StringEncoding]);
+        CFRunLoopRun();
     }
     return 0;
 }
